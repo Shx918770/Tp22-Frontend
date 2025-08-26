3b2772c2e8c909b242df3cd6277efc8b53f95148
shaohongxiang <hsha0055@student.monash.edu>
2025-08-26 15:24:39 +1000

    first commit

---
+++ TP22-Frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Hello Vue.js</h1>
+    <p>This is Tp22 Frontend!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  margin-top: 50px;
+  font-family: Arial, sans-serif;
+}
+</style>

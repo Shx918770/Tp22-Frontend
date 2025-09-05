3488f2539f99963d129f75183a49dbdd408623ad
Shx918770 <hsha0055@student.monash.edu>
2025-09-05 17:32:01 +1000

    add password page

---
+++ TP22-Frontend/src/components/PasswordGate.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="password-gate">
+    <h2>Enter Password</h2>
+    <input v-model="inputPassword" type="password" placeholder="Password" />
+    <button @click="checkPassword">Enter</button>
+    <p v-if="error" style="color:red">{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputPassword: "",
+      error: ""
+    }
+  },
+  methods: {
+    checkPassword() {
+      const correct = "melsustain"  
+      if (this.inputPassword === correct) {
+        localStorage.setItem("siteAccess", "granted")
+        this.$router.push("/")
+      } else {
+        this.error = "Wrong password!"
+      }
+    }
+  }
+}
+</script>
--- TP22-Frontend/src/router/index.js
+++ TP22-Frontend/src/router/index.js
@@ -2,10 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import SocialPage from '../components/social/SocialPage.vue'
 import EnvironmentPage from '../components/environment/EnvironmentPage.vue'
+import PasswordGate from '../components/PasswordGate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/password',
+      name: 'password',
+      component: PasswordGate
+    },
     {
       path: '/',
       name: 'home',
@@ -22,6 +28,15 @@ const router = createRouter({
       component: EnvironmentPage
     }
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const access = localStorage.getItem("siteAccess")
+  if (!access && to.path !== "/password") {
+    next("/password")
+  } else {
+    next()
+  }
 })
 
 export default router
